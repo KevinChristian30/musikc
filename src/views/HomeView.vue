9dfb60cf47d1078327733bfabf5c01b4b3501d37
Kevin Christian <kdotchrist30@gmail.com>
2024-05-21 20:02:26 +0700

    feat: fetched data from backend

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,33 @@
 <template>
   <app-introduction></app-introduction>
 
-  <app-main-content></app-main-content>
+  <app-main-content :songs="songs"></app-main-content>
 </template>
 
 <script>
 import AppIntroduction from '@/components/AppIntroduction.vue';
 import AppMainContent from '@/components/AppMainContent.vue';
-import { defineComponent } from 'vue';
+import { songsCollection } from '@/plugins/firebase';
 
-export default defineComponent({
+export default {
   name: 'HomeView',
   components: {
     AppIntroduction,
     AppMainContent
+  },
+  data() {
+    return {
+      songs: []
+    };
+  },
+  async created() {
+    const snapshots = await songsCollection.get();
+    snapshots.forEach((document) => {
+      this.songs.push({
+        id: document.id,
+        ...document.data()
+      });
+    });
   }
-});
+};
 </script>
