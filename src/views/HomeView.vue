b9d7fcee0cc34c7b9cc7a68b152e6d9207bf08f6
Kevin Christian <kdotchrist30@gmail.com>
2024-05-22 22:00:15 +0700

    feat: song detail page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -17,17 +17,56 @@ export default {
   },
   data() {
     return {
-      songs: []
+      songs: [],
+      maxPerPage: 25,
+      requesting: false
     };
   },
-  async created() {
-    const snapshots = await songsCollection.get();
-    snapshots.forEach((document) => {
-      this.songs.push({
-        id: document.id,
-        ...document.data()
+  methods: {
+    async getSongs() {
+      if (this.requesting) {
+        return;
+      }
+
+      this.requesting = true;
+
+      let snapshots;
+      if (this.songs.length) {
+        const lastDocument = await songsCollection.doc(this.songs[this.songs.length - 1].id).get();
+        snapshots = await songsCollection
+          .orderBy('modified_name')
+          .startAfter(lastDocument)
+          .limit(this.maxPerPage)
+          .get();
+      } else {
+        snapshots = await songsCollection.orderBy('modified_name').limit(this.maxPerPage).get();
+      }
+
+      snapshots.forEach((document) => {
+        this.songs.push({
+          id: document.id,
+          ...document.data()
+        });
       });
-    });
+
+      this.requesting = false;
+    },
+    handleScroll() {
+      const { scrollTop, offsetHeight } = document.documentElement;
+      const innerHeight = window.innerHeight;
+      const bottomOfWindow = Math.round(scrollTop) + innerHeight === offsetHeight;
+
+      if (bottomOfWindow) {
+        this.getSongs();
+      }
+    }
+  },
+  async created() {
+    window.addEventListener('scroll', this.handleScroll);
+    await this.getSongs();
+  },
+  beforeUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
   }
 };
 </script>
